fb07a323e88e43af6f601ed86f7be49e52e67b6c
Error323 <folkerthuizinga@gmail.com>
2009-11-20 13:47:04 +0100

    Added ModuleFactory system

---
+++ src/factories/ModuleFactory.hpp
@@ -0,0 +1,27 @@
+#ifndef MODULE_FACTORY
+#define MODULE_FACTORY
+
+#include <list>
+
+#include "../observers/AObserver.hpp"
+
+class ModuleReleasedObserver;
+class ASubject;
+
+// The idea here is to create a modulefactory with an ModuleReleased observer
+// that lets the factory know when a module can be released again
+template <class Module>
+class ModuleFactory: AObserver {
+	public:
+		ModuleFactory(ModuleReleasedObserver* mro): moduleReleased(mro) {}
+		~ModuleFactory(){}
+
+		Module* Instance();
+	
+	private:
+		std::list<Module*> free;
+		ModuleReleasedObserver *moduleReleased;
+
+		void Update(ASubject *subject);
+
+};
