d2e86cbd497717b2e580ca9721977addab6adbc4
kloot <kloot@kloot0.(none)>
2009-12-07 18:50:33 +0100

    * cleanup

--- src/lua/LuaAICallBackHandler.cpp
+++ src/lua/LuaAICallBackHandler.cpp
@@ -9,26 +9,6 @@
 AIHelper* LuaAICallBackHandler::currHelper = NULL;
 LuaModule* LuaAICallBackHandler::currModule = NULL;
 
-/*
-int LuaAICallBackHandler::Avg(lua_State* L) {
-	// get number of arguments passed to us from Lua
-	int n = lua_gettop(L);
-	float sum = 0;
-
-	for (int i = 1; i <= n; i++) {
-		assert(lua_isnumber(L, i));
-		sum += lua_tonumber(L, i);
-	}
-
-	lua_pushnumber(L, sum / n); // avg
-	lua_pushnumber(L, sum);     // sum
-
-	// return number of arguments to Lua
-	return 2;
-}
-*/
-
-
 int LuaAICallBackHandler::EcoStateIsStallingMetal(lua_State* L) {
 	assert(lua_gettop(L) == 0);
 	lua_pushboolean(L, currHelper->ecoState->IsStallingMetal());
