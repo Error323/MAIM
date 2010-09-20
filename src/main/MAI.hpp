f934fffb99004b0ee0950251ee703d20fd144b0f
Error323 <folkerthuizinga@gmail.com>
2010-09-21 00:27:42 +0200

    CMakeLists.txt update, not working yet

---
+++ src/main/MAI.hpp
@@ -0,0 +1,14 @@
+#ifndef MAI
+#define MAI
+
+#ifdef DEBUG
+	#define NAME   "MAI (DEBUG)"
+#else
+	#define NAME   "MAI"
+#endif
+#define VERSION    "@VERSION@"
+#define HUMAN_NAME NAME " v" VERSION " (" __DATE__ ")"
+#define AI_CODENAME    "MAI Initial - " HUMAN_NAME
+#define AI_CREDITS     "(developed by Error323 and Kloot)"
+
+#endif // MAI
