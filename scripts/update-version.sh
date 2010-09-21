633a303673285a55494c28e57a2ab871d5350b05
Error323 <folkerthuizinga@gmail.com>
2010-09-21 02:52:52 +0200

    update regex for git tags

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -8,5 +8,7 @@ echo "$1" > "$2/VERSION"
 
 FILE="$2/data/AIInfo.lua"
 cp $FILE $FILE.bak
-sed -e "s/[0-9]\.[0-9]\.[0-9]/$1/g" < $FILE.bak > $FILE
+sed -r "s/'[[:digit:]]\.[[:digit:]]\.[[:digit:]]'/'$1'/g" < $FILE.bak > $FILE
+cp $FILE $FILE.bak
+sed -r "s/'[[:alnum:]]*\-[[:digit:]]*\-[[:alnum:]]*'/'$1'/g" < $FILE.bak > $FILE
 rm $FILE.bak
