9d0120d2da39577da45d1ad4146e2fa9909b4716
Error323 <folkerthuizinga@gmail.com>
2010-09-21 11:21:20 +0200

    -

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -8,7 +8,7 @@ echo "$1" > "$2/VERSION"
 
 FILE="$2/data/AIInfo.lua"
 LINE=`grep 'AI version - !This comment is used for parsing!' < $FILE`
-NEWLINE="		value  = '$1', -- AI version - !This comment is used for parsing!"
+NEWLINE="\t\tvalue  = '$1', -- AI version - !This comment is used for parsing!"
 
 cp $FILE $FILE.bak
 sed -r "s/$LINE/$NEWLINE/g" < $FILE.bak > $FILE
